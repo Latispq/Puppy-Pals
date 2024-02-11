4c62e284bec8428b44d5def66f86f43747dda119
Latispq <Latispq@gmail.com>
2024-02-11 18:34:12 -0500

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-
 import './App.css'
+import { puppyList } from './data.js'
+import { useState } from 'react'
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+  console.log(puppies)
   return (
-    <>
-
-    </>
+    <div className='App'>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {
+        puppies.map((puppy) => {
+          return <p onClick={() => { setFeatPupId(puppy.id); }} key={puppy.id}>{puppy.name}</p>
+        })
+      }
+    </div>
   )
 }
 
